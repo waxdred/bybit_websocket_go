7784ca4bf123aaf0770efaaf654f1d9740ed2fe7
waxcoin <waxcoin@hotmail.com>
2023-03-09 00:07:06 +0700

    handler function position order execution greek need add protection of creation connection 500 in 5 min

--- models.go
+++ models.go
@@ -1,10 +1,14 @@
 package main
 
-type Position struct {
+type Topic struct {
 	ID           string `json:"id"`
 	Topic        string `json:"topic"`
 	CreationTime int64  `json:"creationTime"`
-	Data         []struct {
+}
+
+type Position struct {
+	Topic Topic
+	Data  []struct {
 		PositionIdx    int    `json:"positionIdx"`
 		TradeMode      int    `json:"tradeMode"`
 		RiskID         int    `json:"riskId"`
@@ -30,5 +34,120 @@ type Position struct {
 		BustPrice      string `json:"bustPrice"`
 		Category       string `json:"category"`
 		PositionStatus string `json:"positionStatus"`
+	} `json:"data"`
+}
+
+type Execution struct {
+	Topic Topic
+	Data  []struct {
+		Category        string `json:"category"`
+		Symbol          string `json:"symbol"`
+		ExecFee         string `json:"execFee"`
+		ExecID          string `json:"execId"`
+		ExecPrice       string `json:"execPrice"`
+		ExecQty         string `json:"execQty"`
+		ExecType        string `json:"execType"`
+		ExecValue       string `json:"execValue"`
+		IsMaker         bool   `json:"isMaker"`
+		FeeRate         string `json:"feeRate"`
+		TradeIv         string `json:"tradeIv"`
+		MarkIv          string `json:"markIv"`
+		BlockTradeID    string `json:"blockTradeId"`
+		MarkPrice       string `json:"markPrice"`
+		IndexPrice      string `json:"indexPrice"`
+		UnderlyingPrice string `json:"underlyingPrice"`
+		LeavesQty       string `json:"leavesQty"`
+		OrderID         string `json:"orderId"`
+		OrderLinkID     string `json:"orderLinkId"`
+		OrderPrice      string `json:"orderPrice"`
+		OrderQty        string `json:"orderQty"`
+		OrderType       string `json:"orderType"`
+		StopOrderType   string `json:"stopOrderType"`
+		Side            string `json:"side"`
+		ExecTime        string `json:"execTime"`
+		IsLeverage      string `json:"isLeverage"`
+	} `json:"data"`
+}
+
+type Order struct {
+	Topic Topic
+	Data  []struct {
+		Symbol             string `json:"symbol"`
+		OrderID            string `json:"orderId"`
+		Side               string `json:"side"`
+		OrderType          string `json:"orderType"`
+		CancelType         string `json:"cancelType"`
+		Price              string `json:"price"`
+		Qty                string `json:"qty"`
+		OrderIv            string `json:"orderIv"`
+		TimeInForce        string `json:"timeInForce"`
+		OrderStatus        string `json:"orderStatus"`
+		OrderLinkID        string `json:"orderLinkId"`
+		LastPriceOnCreated string `json:"lastPriceOnCreated"`
+		ReduceOnly         bool   `json:"reduceOnly"`
+		LeavesQty          string `json:"leavesQty"`
+		LeavesValue        string `json:"leavesValue"`
+		CumExecQty         string `json:"cumExecQty"`
+		CumExecValue       string `json:"cumExecValue"`
+		AvgPrice           string `json:"avgPrice"`
+		BlockTradeID       string `json:"blockTradeId"`
+		PositionIdx        int    `json:"positionIdx"`
+		CumExecFee         string `json:"cumExecFee"`
+		CreatedTime        string `json:"createdTime"`
+		UpdatedTime        string `json:"updatedTime"`
+		RejectReason       string `json:"rejectReason"`
+		StopOrderType      string `json:"stopOrderType"`
+		TriggerPrice       string `json:"triggerPrice"`
+		TakeProfit         string `json:"takeProfit"`
+		StopLoss           string `json:"stopLoss"`
+		TpTriggerBy        string `json:"tpTriggerBy"`
+		SlTriggerBy        string `json:"slTriggerBy"`
+		TriggerDirection   int    `json:"triggerDirection"`
+		TriggerBy          string `json:"triggerBy"`
+		CloseOnTrigger     bool   `json:"closeOnTrigger"`
+		Category           string `json:"category"`
+	} `json:"data"`
+}
+
+type Wallet struct {
+	Topic Topic
+	Data  []struct {
+		AccountIMRate          string `json:"accountIMRate"`
+		AccountMMRate          string `json:"accountMMRate"`
+		TotalEquity            string `json:"totalEquity"`
+		TotalWalletBalance     string `json:"totalWalletBalance"`
+		TotalMarginBalance     string `json:"totalMarginBalance"`
+		TotalAvailableBalance  string `json:"totalAvailableBalance"`
+		TotalPerpUPL           string `json:"totalPerpUPL"`
+		TotalInitialMargin     string `json:"totalInitialMargin"`
+		TotalMaintenanceMargin string `json:"totalMaintenanceMargin"`
+		Coin                   []struct {
+			Coin                string `json:"coin"`
+			Equity              string `json:"equity"`
+			UsdValue            string `json:"usdValue"`
+			WalletBalance       string `json:"walletBalance"`
+			AvailableToWithdraw string `json:"availableToWithdraw"`
+			AvailableToBorrow   string `json:"availableToBorrow"`
+			BorrowAmount        string `json:"borrowAmount"`
+			AccruedInterest     string `json:"accruedInterest"`
+			TotalOrderIM        string `json:"totalOrderIM"`
+			TotalPositionIM     string `json:"totalPositionIM"`
+			TotalPositionMM     string `json:"totalPositionMM"`
+			UnrealisedPnl       string `json:"unrealisedPnl"`
+			CumRealisedPnl      string `json:"cumRealisedPnl"`
+			Bonus               string `json:"bonus"`
+		} `json:"coin"`
+		AccountType string `json:"accountType"`
+	} `json:"data"`
+}
+
+type Greek struct {
+	Topic Topic
+	Data  []struct {
+		BaseCoin   string `json:"baseCoin"`
+		TotalDelta string `json:"totalDelta"`
+		TotalGamma string `json:"totalGamma"`
+		TotalVega  string `json:"totalVega"`
+		TotalTheta string `json:"totalTheta"`
 	} `json:"data"`
 }
--- wssBybit.go
+++ wssBybit.go
@@ -3,45 +3,157 @@ package main
 import (
 	"crypto/hmac"
 	"crypto/sha256"
+	"encoding/json"
 	"fmt"
 	"log"
+	"sync"
 	"time"
 
 	"github.com/gorilla/websocket"
 )
 
+type SubscribeHandler func(*WssBybit)
+
 type WssBybit struct {
-	conn          *websocket.Conn
-	positionBool  bool
-	executionBool bool
-	orderBool     bool
-	walletBool    bool
-	greekBool     bool
-	position      func([]byte)
-	execution     func([]byte)
-	order         func([]byte)
-	wallet        func([]byte)
-	greek         func([]byte)
-	messages      chan []byte
+	mu       sync.RWMutex
+	priv     private
+	pub      public
+	nbconn   int
+	nbHandle int
 }
 
-func (wss *WssBybit) New(url, apiKey, apiSecret string) *WssBybit {
+type public struct {
+	conn *websocket.Conn
+	recv chan []byte
+	err  chan []byte
+}
+
+type private struct {
+	conn      *websocket.Conn
+	position  SubscribeHandler
+	execution SubscribeHandler
+	order     SubscribeHandler
+	wallet    SubscribeHandler
+	greek     SubscribeHandler
+	recv      chan []byte
+	msg       []byte
+	err       chan []byte
+}
+
+func (wss *WssBybit) AddPrivateSubs(topic string, args []string, subs SubscribeHandler) {
+	wss.mu.Lock()
+	defer wss.mu.Unlock()
+	send := map[string]interface{}{
+		"op":   "subscribe",
+		"args": args,
+	}
+	switch topic {
+	case "position":
+		wss.priv.position = subs
+		wss.priv.conn.WriteJSON(send)
+		wss.nbHandle += 1
+	case "execution":
+		wss.priv.execution = subs
+		wss.priv.conn.WriteJSON(send)
+		wss.nbHandle += 1
+	case "order":
+		wss.priv.order = subs
+		wss.priv.conn.WriteJSON(send)
+		wss.nbHandle += 1
+	case "wallet":
+		wss.priv.wallet = subs
+		wss.priv.conn.WriteJSON(send)
+		wss.nbHandle += 1
+	case "greek":
+		wss.priv.greek = subs
+		wss.priv.conn.WriteJSON(send)
+		wss.nbHandle += 1
+	}
+}
+
+func (wss *WssBybit) RecvPosition() *Position {
+	wss.mu.Lock()
+	defer wss.mu.Unlock()
+	position := Position{}
+	err := json.Unmarshal(wss.priv.msg, &position)
+	if err != nil {
+		return nil
+	}
+	return &position
+}
+
+func (wss *WssBybit) RecvExecution() *Execution {
+	wss.mu.Lock()
+	defer wss.mu.Unlock()
+	execution := Execution{}
+	err := json.Unmarshal(wss.priv.msg, &execution)
+	if err != nil {
+		return nil
+	}
+	return &execution
+}
+
+func (wss *WssBybit) RecvOrder() *Order {
+	wss.mu.Lock()
+	defer wss.mu.Unlock()
+	order := Order{}
+	err := json.Unmarshal(wss.priv.msg, &order)
+	if err != nil {
+		return nil
+	}
+	return &order
+}
+
+func (wss *WssBybit) RecvWallet() *Wallet {
+	wss.mu.Lock()
+	defer wss.mu.Unlock()
+	wallet := Wallet{}
+	err := json.Unmarshal(wss.priv.msg, &wallet)
+	if err != nil {
+		return nil
+	}
+	return &wallet
+}
+
+func (wss *WssBybit) RecvGreek() *Greek {
+	wss.mu.Lock()
+	defer wss.mu.Unlock()
+	greek := Greek{}
+	err := json.Unmarshal(wss.priv.msg, &greek)
+	if err != nil {
+		return nil
+	}
+	return &greek
+}
+
+func (wss *WssBybit) NewPrivate(urlPrivate, apiKey, apiSecret string) *WssBybit {
+	var err error
+	wss.nbconn += 1
+	wss.priv.recv = make(chan []byte)
+	wss.priv.err = make(chan []byte)
 	sign, expires := generateSignature(apiKey, apiSecret)
 	// Create websocket connection.
-	conn, _, err := websocket.DefaultDialer.Dial(url, nil)
+	wss.priv.conn, _, err = websocket.DefaultDialer.Dial(url, nil)
 	if err != nil {
 		log.Fatal("connection failed:", err)
 	}
 	go func() {
 		for {
-			_, message, err := conn.ReadMessage()
+			data := make(map[string]interface{})
+			_, message, err := wss.priv.conn.ReadMessage()
 			if err != nil {
 				log.Println("error reading message from WebSocket:", err)
-				close(wss.messages)
+				close(wss.priv.recv)
 				return
 			}
-			fmt.Println(string(message))
-			wss.messages <- message
+			json.Unmarshal(message, &data)
+			if value, ok := data["success"].(bool); ok {
+				if !value {
+					wss.priv.err <- message
+				}
+			}
+			// fmt.Println(string(message))
+			wss.priv.recv <- message
 		}
 	}()
 	// Authenticate with API.
@@ -49,29 +161,55 @@ func (wss *WssBybit) New(url, apiKey, apiSecret string) *WssBybit {
 		"op":   "auth",
 		"args": []interface{}{apiKey, expires, sign},
 	}
-	err = conn.WriteJSON(authPayload)
+	err = wss.priv.conn.WriteJSON(authPayload)
 	if err != nil {
 		log.Fatal("authentication failed:", err)
 	}
 	fmt.Println("authentication successful")
 	// Start goroutine to receive messages from WebSocket.
+	go func() {
+		ticker := time.NewTicker(time.Duration(time.Second * 20))
+		defer ticker.Stop()
+		for {
+			select {
+			case <-ticker.C:
+				wss.mu.Lock()
+				heartbeat := map[string]interface{}{
+					"op":   "subscribe",
+					"args": []string{"ping"},
+				}
+				wss.priv.conn.WriteJSON(heartbeat)
+				wss.mu.Unlock()
+			case err := <-wss.priv.err:
+				panic(string(err))
+			case message := <-wss.priv.recv:
+				data := make(map[string]interface{})
+				err := json.Unmarshal(message, &data)
+				if err != nil {
+					log.Println("error decoding message:", err)
+					continue
+				}
+				if topic, ok := data["topic"]; ok {
+					switch topic {
+					case "position":
+						wss.priv.position(wss)
+					case "execution":
+					case "order":
+					case "wallet":
+						wss.priv.wallet(wss)
+					case "greek":
+					}
+				}
+			default:
+			}
+		}
+	}()
 	return wss
 }
 
-func (wss *WssBybit) Subscribe(subscribe ...string) {
-	for _, sub := range subscribe {
-		switch sub {
-		case "postion":
-			wss.positionBool = true
-		default:
-			fmt.Println("Subscribe not found")
-		}
-	}
-}
-
 func (wss *WssBybit) Close() error {
-	if wss.conn != nil {
-		return wss.conn.Close()
+	if wss.priv.conn != nil {
+		return wss.priv.conn.Close()
 	}
 	return nil
 }
